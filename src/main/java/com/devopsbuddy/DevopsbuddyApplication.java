55bbb15aba4155ef64ea51a90d8df73812c95c9f
iansethcolin <ianseth.colin@gmail.com>
2016-09-27 22:41:10 -0700

    Added Bootstrap and Jquery dependecies to POM

---
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -0,0 +1,17 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by iansethcolin on 9/27/16.
+ */
+@SpringBootApplication
+public class DevopsbuddyApplication {
+
+    public static void main(String[] args){
+    SpringApplication.run(DevopsbuddyApplication.class,args);}
+
+    }
